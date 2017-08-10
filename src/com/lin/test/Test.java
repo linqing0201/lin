7b4c67a6d3e30d858e0bd23bbfea1278c313bb4a
LIN <LIN@LIN>
2017-08-10 20:04:25 +0800

    test

---
+++ src/com/lin/test/Test.java
@@ -0,0 +1,12 @@
+package com.lin.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 1000000000; i++) {
+			System.out.println("hello:-----"+i);
+		}
+
+	}
+
+}
